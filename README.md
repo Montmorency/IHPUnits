1d76eb9646bf93be7f59771cf58a0e3184dd3101
Montmorency <lamberh@tcd.ie>
2020-09-04 19:22:51 +0100

    updated IHP revision.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Web view of haskell units package.
+
+For example, say I have a Piece of String which is 1 meter long and I would like to tell my father about it.
+He refuses to use the metric system for any domestic or familial related matter
+so I have to convert the Quantity associated with the Length Dimension of the Piece of String
+into the Imperial System of Measurement.
+That means I must take the Quantity represented by (1 meter), (numerical value, unit), and convert it to
+a new Quantity (3.28084 feet). Note both Quantities describe an identical Physical 
+Invariant: the Length of the Piece of String.
+Now I can write a letter to my father telling him about my Piece of String. 
--- Web/View/Static/Units.hs
+++ Web/View/Static/Units.hs
@@ -13,30 +13,26 @@ instance View UnitsView ViewContext where
         <div class = "intro">
             <h1> Type Safe Units </h1>
             <p> 
-               This page is a snapshot of  
-               <a href="https://github.com/goldfirere"> goldfirere's </a>
-               Haskell <a href="https://github.com/goldfirere/units"> units </a> package.
-               The <a href="https://github.com/goldfirere/units"> units </a> package is meant to 
-               facilitate compile time dimensional analysis for any program
-               which describes physically (or otherwise) dimensioned quantities. 
-               At the moment this site
-               only exposes the physically dimensioned aspects 
-               of the units package. 
+                This page is a snapshot of  
+                <a href="https://github.com/goldfirere"> goldfirere's </a>
+                Haskell <a href="https://github.com/goldfirere/units"> units </a> package.
+                The <a href="https://github.com/goldfirere/units"> units </a> package is meant to 
+                facilitate compile time dimensional analysis for any program
+                which describes physically (or otherwise) dimensioned quantities. 
+                At the moment this site
+                only exposes the physically dimensioned aspects 
+                of the units package. 
             </p>
             <p>
-               Let us fix some ideas and terminology. 
-               A Quantity is a numerical value <em> and </em> its associated unit of measurement.
-               A Quantity describes an underlying Physically Invariant Property with which we can associate some
-               pure or composite Dimension. Quantities associated with particular Dimensions can be expressed
-               in any system of units one pleases. 
-               For example, say I have a Piece of String which is 1 meter long and I would like to tell my father about it.
-               He refuses to use the metric system for any domestic or familial related matter
-               so I have to convert the Quantity associated with the Length Dimension of the Piece of String
-               into the Imperial System of Measurement.
-               That means I must take the Quantity represented by (1 meter), (numerical value, unit), and convert it to
-               a new Quantity (3.28084 feet). Note both Quantities describe an identical Physical 
-               Invariant: the Length of the Piece of String.
-               Now I can write a letter to my father telling him about my Piece of String. 
+                Let us fix some ideas and terminology. 
+                A Quantity is a numerical value <em> and </em> its associated unit of measurement.
+                A Quantity describes an underlying Physically Invariant Property which we can associate with some
+                pure or composite Dimension; e.g., a pure dimension like the Length of a piece of string, 
+                or the composite dimension of irradiance which has dimensions of Energy per Time per Area. 
+                Quantities associated with particular Dimensions can be expressed
+                in any system of units one pleases e.g., meters and feet for a length dimension, or,
+                Joules per Second per Square Meter or British Thermal Units per Week per Square Yard 
+                for our irradiance. There is a great deal of freedom in the way we measure dimensions.
             </p>
             <p>   
                 Hopefully this package/site will eventually be of use to:
@@ -65,7 +61,10 @@ instance View UnitsView ViewContext where
                 divide and multiply, with that order of precedence (divide and multiply sharing the same level
                 of precedence). So, example, <samp> m/s^2 </samp> and 
                 (m/s s) are the same (seconds exponentiated and seconds juxtaposed) unit, 
-                but <samp> m/s*s = (m/s)*s = m </samp>. 
+                but <samp> m/s*s = (m/s)*s = m </samp>. See the units-defs package for the 
+                systems that are defined. Whereever a unit does not have a standard string representation
+                or there is some ambiguity in the prefixes the user may pass them in as a map in the form
+                ("kJ", Kilo :@ Joule).
             </p>
         </div>
         <div class = "units-convert"> 
@@ -81,17 +80,17 @@ instance View UnitsView ViewContext where
                       <input type="text" class="form-control" id="inputUnitQuantity" placeholder="pure Unit (e.g. m/s)">
                     </div>
                     <div class="form-group col-md-2">
-                      <label for="outputNumericQuantity">Input Unit</label>  
-                      <input type="text" class="form-control" id="outputNumericQuantity" placeholder="-.-" > <!--should be readonly -->
+                      <label for="outputNumericQuantity">Output Number</label>  
+                      <input type="text" class="form-control" id="outputNumericQuantity" placeholder="-.-" readonly="readonly">
                     </div>
                     <div class="form-group col-md-2">
-                      <label for="outputUnitQuantity">Input Unit</label>  
+                      <label for="outputUnitQuantity"> Output Unit</label>  
                       <input type="text" class="form-control" id="outputUnitQuantity" placeholder="pure Unit (e.g. m/s)">
                     </div>
                   </div>
                   <div class="form-group">
-                     <label for="inputAddress">Symbol Table</label>
-                     <input type="text" class="form-control" id="symbolTable" placeholder="(kg, Kilo :@ Gram)">
+                      <label for="inputAddress">Symbol Table</label>
+                      <input type="text" class="form-control" id="symbolTable" placeholder="(kg, Kilo :@ Gram)">
                   </div>
                   <button type="submit" class="btn btn-primary"> Convert  </button>
                 </form>
